68eb51f43a162b19730a76cccf3b3e3ed2354f98
Michael Koval <mkoval3@gatech.edu>
2017-06-05 23:51:42 -0400

    recapped 25 fps

--- public/game.js
+++ public/game.js
@@ -14,7 +14,7 @@ var players;
 var direction = {up: false, down: false, left: false, right: false};
 var directionLoop;
 function setup() {
-
+    frameRate(25);
     socket = io()
     socket.on('game-pending', waitingRoom);
     socket.on('game-start', startGame);
